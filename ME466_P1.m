15b4300de28d299a6fb1a41669e0f814a58ceab8
RobotDoktor <jchanbusiness@gmail.com>
2018-04-08 23:32:43 -0400

    Update ME466_P1.m

--- ME466_P1.m
+++ ME466_P1.m
@@ -17,21 +17,26 @@ F=-T_W(2)/T_F(2)
 %% Buckling
 S_y=303*10^6; %Pa
 S_ut=365*10^6; %Pa
-
+%d_shaft=[0.0254 0.03 0.04 0.05];
 d_shaft=0.01
 a_shaft=pi.*d_shaft.^2./4;
 
 %% Pressure
 pressure_i=F./a_shaft;
-pressure_ext=101*10^3;
+pressure_ext=0;
 thickness=0.005;
 r_in=d_shaft./2;
 r_out=r_in+thickness;
 r=(r_out+r_in)./2;
 
-axial_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)/(r_out.^2-r_in.^2)
-tangential_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)./(r_out.^2-r_in.^2)+(r_in.^2.*r_out.^2.*(pressure_i-pressure_ext))./(r.*(r_out.^2-r_in.^2))
-radial_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)./(r_out.^2-r_in.^2)-(r_in.^2.*r_out.^2.*(pressure_i-pressure_ext))./(r.*(r_out.^2-r_in.^2))
+% axial_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)/(r_out.^2-r_in.^2)
+% tangential_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)./(r_out.^2-r_in.^2)+(r_in.^2.*r_out.^2.*(pressure_i-pressure_ext))./(r.*(r_out.^2-r_in.^2))
+% radial_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)./(r_out.^2-r_in.^2)-(r_in.^2.*r_out.^2.*(pressure_i-pressure_ext))./(r.*(r_out.^2-r_in.^2))
+axial_stress=pressure_i.*r_in.^2./(r_out.^2-r_in.^2)
+tangential_stress=r_in.^2.*pressure_i./(r_out.^2-r_in.^2).*(1+r_out.^2./r.^2)
+radial_stress=r_in.^2.*pressure_i./(r_out.^2-r_in.^2).*(1-r_out.^2./r.^2)
+
+
 
 state=[radial_stress 0 0; 0 tangential_stress 0; 0 0 axial_stress];
 omega1=flipud(eig(state))
@@ -39,4 +44,5 @@ omega_1p=sqrt(omega1(1,1)^2+omega1(2,1)^2+omega1(3,1)^2-(omega1(1,1).*omega1(2,1
 
 N_n=S_y./omega_1p %Factor of safety calculations based on distortion energy theorem
 N_s=S_ut.*0.577./(abs(omega1(1,1)-omega1(3,1))./2)
+min(N_n,N_s)
 
