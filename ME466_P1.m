e02e21fb482f0307cf07942e2bea6f2f64070e6b
RobotDoktor <jchanbusiness@gmail.com>
2018-04-09 00:06:17 -0400

    Update ME466_P1.m

--- ME466_P1.m
+++ ME466_P1.m
@@ -17,15 +17,19 @@ F=-T_W(2)/T_F(2)
 %% Buckling
 S_y=303*10^6; %Pa
 S_ut=365*10^6; %Pa
-%d_shaft=[0.0254 0.03 0.04 0.05];
-d_shaft=0.01
-a_shaft=pi.*d_shaft.^2./4;
+
+d_shaft=[0.01 0.02 0.03 0.04];
+
 
 %% Pressure
+
+for i=1:length(d_shaft)
+
+a_shaft=pi.*d_shaft(i).^2./4;
 pressure_i=F./a_shaft;
 pressure_ext=0;
 thickness=0.005;
-r_in=d_shaft./2;
+r_in=d_shaft(i)./2;
 r_out=r_in+thickness;
 r=(r_out+r_in)./2;
 
@@ -40,9 +44,16 @@ radial_stress=r_in.^2.*pressure_i./(r_out.^2-r_in.^2).*(1-r_out.^2./r.^2)
 
 state=[radial_stress 0 0; 0 tangential_stress 0; 0 0 axial_stress];
 omega1=flipud(eig(state))
-omega_1p=sqrt(omega1(1,1)^2+omega1(2,1)^2+omega1(3,1)^2-(omega1(1,1).*omega1(2,1)+omega1(2,1).*omega1(3,1)+omega1(1,1).*omega1(3,1)))
+omega_1p=sqrt(omega1(1,1)^2+omega1(2,1)^2+omega1(3,1)^2-(omega1(1,1).*omega1(2,1)+omega1(2,1).*omega1(3,1)+omega1(1,1).*omega1(3,1)));
 
-N_n=S_y./omega_1p %Factor of safety calculations based on distortion energy theorem
-N_s=S_ut.*0.577./(abs(omega1(1,1)-omega1(3,1))./2)
-min(N_n,N_s)
+N_n=S_y./omega_1p; %Factor of safety calculations based on distortion energy theorem
+N_s=S_ut.*0.577./(abs(omega1(1,1)-omega1(3,1))./2);
+FoS(i)=min(N_n,N_s)
+i=i+1;
+end
 
+figure
+plot(d_shaft,FoS)
+title('Relationship between Pressure factor of safety and piston diameter')
+xlabel('Piston Diameter (m)')
+ylabel('Factor of Safety')
