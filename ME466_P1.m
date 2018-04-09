cefe394c21580e2c7fa2e22118fab15771f2e8d6
RobotDoktor <jchanbusiness@gmail.com>
2018-04-09 18:52:40 -0400

    Update ME466_P1.m

--- ME466_P1.m
+++ ME466_P1.m
@@ -15,14 +15,48 @@ T_F=cross(f_pos, f_dir); %torque vector due to piston force divided by force mag
 F=-T_W(2)/T_F(2)
 
 %% Buckling
-S_y=303*10^6; %Pa
-S_ut=303*10^6; %Pa
+S_y=207*10^6; %Pa
 
-d_shaft=0.005:0.001:0.05;;
+d_shaft=0.005:0.001:0.05;
 
 
 %% Pressure
+thickness_=0.001:0.001:0.01;
+for j=1:length(thickness_)
+d_shaft=0.04;
+r_in=d_shaft./2;
+a_shaft=pi.*d_shaft.^2./4;
+pressure_i=F./a_shaft;
 
+r_out=r_in+thickness_(j);
+r=(r_out+r_in)./2;
+
+% axial_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)/(r_out.^2-r_in.^2)
+% tangential_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)./(r_out.^2-r_in.^2)+(r_in.^2.*r_out.^2.*(pressure_i-pressure_ext))./(r.*(r_out.^2-r_in.^2))
+% radial_stress=(pressure_i.*r_in.^2-pressure_ext.*r_out.^2)./(r_out.^2-r_in.^2)-(r_in.^2.*r_out.^2.*(pressure_i-pressure_ext))./(r.*(r_out.^2-r_in.^2))
+axial_stress=pressure_i.*r_in.^2./(r_out.^2-r_in.^2);
+tangential_stress=r_in.^2.*pressure_i./(r_out.^2-r_in.^2).*(1+r_out.^2./r.^2);
+radial_stress=r_in.^2.*pressure_i./(r_out.^2-r_in.^2).*(1-r_out.^2./r.^2);
+
+
+
+state=[radial_stress 0 0; 0 tangential_stress 0; 0 0 axial_stress];
+omega1=flipud(eig(state))
+omega_1p=sqrt(omega1(1,1)^2+omega1(2,1)^2+omega1(3,1)^2-(omega1(1,1).*omega1(2,1)+omega1(2,1).*omega1(3,1)+omega1(1,1).*omega1(3,1)));
+
+N_n=S_y./omega_1p; %Factor of safety calculations based on distortion energy theorem
+N_s=S_y.*0.577./(abs(omega1(1,1)-omega1(3,1))./2);
+FoSc(j)=min(N_n,N_s)
+
+end
+
+figure
+plot(thickness_,FoSc)
+title('Relationship between Pressure factor of safety and piston diameter')
+xlabel('Cylinder Thickness (m)')
+ylabel('Factor of Safety')
+
+d_shaft=0.005:0.001:0.05;
 for i=1:length(d_shaft)
     
 r_in=d_shaft(i)./2;
@@ -57,3 +91,6 @@ plot(d_shaft,FoS)
 title('Relationship between Pressure factor of safety and piston diameter')
 xlabel('Piston Diameter (m)')
 ylabel('Factor of Safety')
+
+
+
